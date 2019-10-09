6b26a482194a74cbe9e2a295dad858ad3273229d
Joseph Amar <joseph.amar1391@gmail.com>
2019-10-09 17:43:03 -0500

    idk i'm tired. if it's broken go back

--- dev/fluor/xlocs.py
+++ dev/fluor/xlocs.py
@@ -12,6 +12,13 @@ D = [0, 10000, 20000, 30000, 50000, 80000, 100000, 110000, 120000, 150000, 16000
 y1 = [2000, 2000, 0, 0, 500, 800, 1200, 1300, 1350, 1800, 2000, 3250, 4000]
 x1 = [0]
 
+pumpA = {'Weight': 4500, 'Length': 7, 'Width': 9, 'Cost': 3000000}
+pumpB = {'Weight': 7000, 'Length': 10, 'Width': 12, 'Cost': 4250000}
+pumpC = {'Weight': 8500, 'Length': 12, 'Width': 14, 'Cost': 2400000}
+pumpD = {'Weight': 7500, 'Length': 10, 'Width': 10, 'Cost': 2250000}
+pumpE = {'Weight': 10000, 'Length': 15, 'Width': 15, 'Cost': 3600000}
+pumpF = {'Weight': 4000, 'Length': 6, 'Width': 8, 'Cost': 2000000}
+
 for i in range(len(D)-1):
     x_i = ((D[i+1]-D[i])**2 - (y1[i+1]-y1[i])**2)**(1/2) + x1[i]
     x1.append(x_i)
@@ -26,7 +33,7 @@ b_press = {'Silty Sand': 2000, 'Stable Rock': 6000, 'Medium Dense Sand': 2500, '
 exc_cost = {'Silty Sand': 1, 'Stable Rock': 3, 'Medium Dense Sand': 1, 'Firm Clay': 1}
 fnd_depth = {'Silty Sand': 4, 'Stable Rock': 2, 'Medium Dense Sand': 3, 'Firm Clay': 3}
 
-# plt.plot(x,y)
+plt.plot(x,y)
 
 earth = []
 
@@ -39,6 +46,5 @@ for i in x:
         earth.append((i,'Medium Dense Sand'))
     else:
         earth.append((i,'Firm Clay'))
-
 
 
