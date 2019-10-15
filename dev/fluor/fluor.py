e4ba69b40790385a4b6c4f50499ce4caf7236945
Joseph Amar <joseph.amar1391@gmail.com>
2019-10-14 20:17:47 -0500

    switching computers

--- dev/fluor/fluor.py
+++ dev/fluor/fluor.py
@@ -1,10 +1,306 @@
-#!bin/usr/env
+#!usr/bin/env
+"""
+Created on Sat Oct  5 15:19:17 2019
+
+@author: Joe
+"""
 
 from matplotlib import pyplot as plt
 import numpy as np
 
-x = np.linspace(0,200000,201)
+# Distance between junctions:
+D1 = [0, 10000, 20000, 30000,
+	 50000, 80000, 100000, 110000,
+	 120000, 150000, 160000, 180000, 200000]
+
+# Elevation at each junction:
+y1 = [2000, 2000, 0, 0,
+	  500, 800, 1200, 1300,
+	  1350, 1800, 2000, 3250, 4000]
+
+# Initial x-value at start:
+x1 = [0]
+
+# Derive x-values at junctions based on D and y:
+for i in range(len(D1)-1):
+	x_i = ((D1[i+1]-D1[i])**2 - (y1[i+1]-y1[i])**2)**(1/2) + x1[i]
+	x1.append(x_i)
+
+# print(x1)
+
+dx = 10  # size of sample along x-axis [ft]
+n = (200000/dx)+1
+
+pumpA = {'Name': 'A', 'Weight': 4500, 'Length': 7,
+		 'Width': 9, 'Cost': 3000000,
+		 'NPSHA': 400, 'hp': 7000}
+
+pumpB = {'Name': 'B', 'Weight': 7000, 'Length': 10,
+		 'Width': 12, 'Cost': 4250000,
+		 'NPSHA': 200, 'hp': 8000}
+
+pumpC = {'Name': 'C', 'Weight': 8500, 'Length': 12,
+		 'Width': 14, 'Cost': 2400000,
+		 'NPSHA': 50, 'hp': 4000}
+
+pumpD = {'Name': 'D', 'Weight': 7500, 'Length': 10,
+		 'Width': 10, 'Cost': 2250000,
+		 'NPSHA': 100, 'hp': 6500}
+
+pumpE = {'Name': 'E', 'Weight': 10000, 'Length': 15,
+		 'Width': 15, 'Cost': 3600000,
+		 'NPSHA': 100, 'hp': 7500}
+
+pumpF = {'Name': 'F', 'Weight': 4000, 'Length': 6,
+		 'Width': 8, 'Cost': 2000000,
+		 'NPSHA': 50, 'hp': 3500}
+
+pumps = [pumpA, pumpB, pumpC, pumpD, pumpE, pumpF]
+
+# create n equally spaced partitions on x:
+x = np.linspace(min(x1), max(x1), int(n))
+
+# use more precise values to interpolate y and D at x:
+y = np.interp(x,x1,y1)
+D = np.interp(x,x1,D1)
+
+locations = list(zip(x,y,D))  # y values are not unique but x and D are
+
+# initialize dictionaries:
+slope = {'Silty Sand': 1,
+		 'Stable Rock': 10,
+		 'Medium Dense Sand': .66667,
+		 'Firm Clay': 1.3333} # max slope of soil type [v/h]
+
+b_press = {'Silty Sand': 2000,
+		   'Stable Rock': 6000,
+		   'Medium Dense Sand': 2500,
+		   'Firm Clay': 1500} # b_press == max soil bearing pressure [psi]
+
+exc_cost = {'Silty Sand': 1,
+			'Stable Rock': 3,
+			'Medium Dense Sand': 1,
+			'Firm Clay': 1} # Excavation cost [$]
+
+fnd_depth = {'Silty Sand': 4,
+			 'Stable Rock': 2,
+			 'Medium Dense Sand': 3,
+			 'Firm Clay': 3} # Foundation Depth [ft]
+
+weight = {'Silty Sand': 99,
+		  'Stable Rock': 167,
+		  'Medium Dense Sand': 116,
+		  'Firm Clay': 106} # unit weight of soil [lb/ft3]
+
+# plt.plot(x,y)
+
+earth = []
+
+# Assigns soil type to each discrete "chunk" along x, based on distance along pipeline:
+for i in locations:
+	if i[2] < 20001 or (120001 < i[2] < 150001):
+		earth.append((i,'Silty Sand'))
+	elif i[2] < 80001 or i[2] > 180001:
+		earth.append((i,'Stable Rock'))
+	elif i[2] < 120001:
+		earth.append((i,'Medium Dense Sand'))
+	else:
+		earth.append((i,'Firm Clay'))
+
+# initializes list of excavations based on starting location
+exc = []
 
 
+for i,j in enumerate(x[3000:-1050]): # for each starting location between 30000 and 189500:
+	a = i + int(30000/dx)            # a = starting location index
+	b = a + int(500/dx)              # b = ending location index (500 [ft] away)
+	y0 = y[a]                        # the constant used as the base axis for integration
+#	yb = y[i+500]
+	A = 0                            # Accumulator for the area to be made flat
+	B = 0                            # Accumulator for the area to be sloped based on soil type
+	counter = 0
+	if slope[earth[a][1]] == slope[earth[b][1]]:
+		A = (y[b]-y[a])*500/2
+		# if earth[a][1] == 'Stable Rock':
+		# 	A *= 3
+		while y0 < y[b+counter]:
+			temp = (y[a+counter] - y0)*dx
+			# if earth[a+counter][1] == 'Stable Rock':
+			# 	temp *= 3
+			B += temp
+			y0 += (slope[earth[a + counter][1]])*dx
+			counter += 1
+		exc.append((A+B,a))
+
+	else:
+		for section in range(a,b):
+			temp = ((1/2)*(y[section]+y[section+1])-y[a])*dx
+			# if earth[section][1] == 'Stable Rock':
+			# 	temp *= 3
+			A += temp
+		while y0 < y[a+counter]:
+			temp = (y[a+counter] - y0)*dx
+			# if earth[a+counter][1] == 'Stable Rock':
+			# 	temp *= 3
+			B += temp
+			y0 += (slope[earth[a + counter][1]])*dx
+			counter += 1
+		exc.append((A+B,a))
+
+x_new = []    # initialize truncated list for x range [ft]
+y_new = []    # initialize list of excavation values [ft2]
+
+for i in exc:
+	x_new.append(i[1])
+	y_new.append(i[0])
+
+#plt.plot(x_new,y_new)
+
+# now I need to throw out all the values that occur between junctions,
+# since the only viable pump location (currently) is at a junction
+
+# Junctions occur at D1 locations:
+# D1 = [0, 10000, 20000, 30000,
+# 	 50000, 80000, 100000, 110000,
+# 	 120000, 150000, 160000, 180000, 200000]
+
+# we consider each junction as a potential pump location:
+# the first 4 indices can be thrown out; they are within 30000 [ft] of start
+# the last 2 indices can be thrown out; pressure drop is too much by then (negative NPSHA)
+
+track_hoe  = 2700 #[ft3/day]
+cost_th    = 5000 #[$/day]
+dump_truck = 810  #[ft3/day]
+cost_dt    = 1000 #[$/day]
 
 
+juncs = [50000, 80000, 100000, 110000, 120000, 150000, 160000]
+
+# lowest starting location that touches each junction (determined visually):
+a = [4990, 7974, 10000, 11000, 11950, 14950, 15950]
+jxdex = [5000, 8000, 10000, 11000, 12000, 15000, 16000] # index at which junction lives
+
+
+npsha = [2197, 1652, 1284, 839, 717, 644, 127] # npsha for pump at given junction
+hp_req = 6545  # one pump system, hp requirement is constant
+
+temp_list = list(zip(juncs,npsha))
+n_juncs = [] # initialize enumerated list of each junction and the corresponding npsha
+
+for i,j in enumerate(temp_list):
+	n_juncs.append((i+4, j[0], j[1])) # (i+4) because the first available junction is #4
+
+viable_pumps = []
+
+for junction in n_juncs:
+	for pump in pumps:
+		if pump['NPSHA'] < junction[2] and pump['hp'] > hp_req:
+			viable_pumps.append((junction[0],pump))
+
+exc_costs = []
+for i in a:
+	excavation = exc[i-3000][0] * 500  #[ft3]
+
+	if (excavation % track_hoe) != 0:
+		hoe_days = (excavation//track_hoe) + 1
+	else:
+		hoe_days = excavation/track_hoe
+
+	if earth[i][1] == 'Stable Rock':
+		hoe_cost = hoe_days * cost_th * 3
+	else:
+		hoe_cost = hoe_days * cost_th
+
+	if (excavation % dump_truck) != 0:
+		truck_days = (excavation//dump_truck) + 1
+	else:
+		truck_days = excavation/dump_truck
+
+	truck_cost = truck_days * cost_dt
+	cost = hoe_cost + truck_cost
+	exc_costs.append((cost,i))
+
+pedestal_dimensions = [] # list of dicts with jx, pump, Hp, Lp, Wp, H, Lf, Wf
+unit_weight_concrete = 145 # [lb/ft3]
+
+for jx,pump in viable_pumps:
+	i = jx - 4
+	type_soil = earth[jxdex[i]][1]
+	unit_weight_soil = weight[type_soil]
+	H = 1.5
+	found_depth = fnd_depth[type_soil]
+	pump_weight = pump['Weight']
+	Lp = pump['Length'] + 1
+	Wp = pump['Width'] + 1
+	Hp = found_depth - .5
+	Wf = Wp  # initializing value (guess) for footing width
+	Lf = Lp  # initializing value (guess) for footing length
+	vol_footing  = Wf * Lf * H
+	vol_pedestal = Wp * Lp * Hp
+	Ap = Wp * Lp # Area of pedestal also represents volume of exposed pedestal (height = 1)
+	weight_soil = (vol_footing + vol_pedestal - Ap) * unit_weight_soil
+	Af = Wf*Lf
+	weight_foundation = (pump_weight + (vol_footing * unit_weight_concrete) +
+						 (vol_pedestal * unit_weight_concrete) - weight_soil)
+
+	while (weight_foundation/Af) > b_press[type_soil]:
+		Wf += .01
+		Lf += .01
+
+	if (Wf - 1) < Wp:
+		Lp = Lf
+		Wp = Wf
+		H = 0
+		Hp += 1.5
+		vol = Wp * Lp * Hp
+
+		while (weight_foundation/Af) > b_press[type_soil]:
+			vol = Wp * Lp * Hp
+			weight_foundation = pump_weight + (vol * unit_weight_concrete)
+			Wp += .01
+			Lp += .01
+
+	pedestal_dimensions.append({'Junction': jx, 'Pump': pump['Name'], 'PumpCost': pump['Cost'],
+								'Vol_f': vol + (Wf*Lf*H), 'H_p': Hp,
+								'L_p': Lp, 'W_p': Wp, 'H_f': H, 'L_f': Lf, 'W_f': Wf})
+
+accrued_list = [] # list of tuples: (jx, pump, vol_fnd, vol_exc, vol_moved, vol_trucked, cost_cum)
+
+concrete_truck = 243   # [ft3/truck]
+cost_ct        = 15000 # [$/day]
+
+for option in pedestal_dimensions:
+	junc = option['Junction']
+	i = junc - 4
+	soil = earth[jxdex[i]][1]
+	m = slope[soil]
+	h = fnd_depth[soil]
+	d = h/m
+	W_f = option['W_f']
+	L_f = option['L_f']
+	vol_exc = h * (W_f + 2 + d) * (L_f + 2)
+	vol_moved = 2 * vol_exc - option['Vol_f'] + (W_f * L_f)
+	vol_trucked = option['Vol_f'] - (W_f * L_f)
+
+	if (vol_moved % track_hoe) != 0:
+		hoe_days = (vol_moved//track_hoe) + 1
+	else:
+		hoe_days = vol_moved/track_hoe
+
+	if soil == 'Stable Rock':
+		hoe_cost = hoe_days * cost_th * 3
+	else:
+		hoe_cost = hoe_days * cost_th
+
+	if (vol_trucked % dump_truck) != 0:
+		truck_days = (vol_trucked//dump_truck) + 1
+	else:
+		truck_days = vol_trucked/dump_truck
+
+	num_conc_trucks = (option['Vol_f']//243) + 1
+	cost_conc_trucks = num_conc_trucks * cost_ct
+
+	truck_cost = truck_days * cost_dt
+	cost = hoe_cost + truck_cost + exc_costs[option['Junction']-4][0] + cost_conc_trucks + option['PumpCost']
+
+	accrued_list.append((junc,option['Pump'],cost))
--- dev/fluor/pipes.py
+++ dev/fluor/pipes.py
@@ -3,136 +3,137 @@ import numpy as np
 from itertools import combinations
 
 junctions = [0,
- 10000,
- 20000,
- 30000,
- 50000,
- 80000,
- 100000,
- 110000,
- 120000,
- 150000,
- 160000,
- 180000,
- 200000]
+             10000,
+             20000,
+             30000,
+             50000,
+             80000,
+             100000,
+             110000,
+             120000,
+             150000,
+             160000,
+             180000,
+             200000]
 
 sections = []
 
-for i in range(len(junctions)-1):
-	length = junctions[i+1]-junctions[i]
-	sections.append(length)
-	
+for i in range(len(junctions) - 1):
+    length = junctions[i + 1] - junctions[i]
+    sections.append(length)
+
 P_stat_max = [
-		[114.5, 960.2, 960.2, 1960.4, 1724.6, 1565.7,
-		 1373.1, 1320.1, 1288.8, 1064.9, 968.6, 408.4],
-   
-        [114.5, 960.2, 960.2, 950.5, 1724.6, 1565.7,
-		 1373.1, 1320.1, 1288.8, 1064.9, 968.6, 408.4],
-		 
-        [114.5, 960.2, 960.2, 950.5, 714.7, 1565.7,
-		 1373.1, 1320.1, 1288.8, 1064.9, 968.6, 408.4],
-		 
-		[114.5, 960.2, 960.2, 950.5, 714.7, 555.7,
-		 1373.1, 1320.1, 1288.8, 1064.9, 968.6, 408.4],
-   
-        [114.5, 960.2, 960.2, 950.5, 714.7, 555.7,
-		 363.2, 1320.1, 1288.8, 1064.9, 968.6, 408.4],
-		 
-	    [114.5, 960.2, 960.2, 950.5, 714.7, 555.7,
-		 363.2, 310.2, 1288.8, 1064.9, 968.6, 408.4],
-	  
-	    [114.5, 960.2, 960.2, 950.5, 714.7, 555.7,
-		 363.2, 310.2, 278.8, 1064.9, 968.6, 408.4]
-   		]
+    [114.5, 960.2, 960.2, 1960.4, 1724.6, 1565.7,
+     1373.1, 1320.1, 1288.8, 1064.9, 968.6, 408.4],
 
-schedules = [{'Name': 'Schedule 5', 'ID': 29.5, 't': .5/2,
-			  'Area': ((15**2)-((29.5/2)**2))*np.pi},
-			 {'Name': 'Schedule 10', 'ID': 29.376, 't': .624/2,
-			  'Area': ((15**2)-((29.376/2)**2))*np.pi},
-			 {'Name': 'STD', 'ID': 29.25, 't': .75/2,
-			  'Area': ((15**2)-((29.25/2)**2))*np.pi},
-			 {'Name': 'Schedule 20', 'ID': 29, 't': 1/2,
-			  'Area': ((15**2)-((29/2)**2))*np.pi},
-			 {'Name': 'Schedule 30', 'ID': 28.75, 't': 1.25/2,
-			  'Area': ((15**2)-((28.75/2)**2))*np.pi}]
+    [114.5, 960.2, 960.2, 950.5, 1724.6, 1565.7,
+     1373.1, 1320.1, 1288.8, 1064.9, 968.6, 408.4],
 
-t_vals = [.5/2, .624/2, .75/2, 1/2, 1.25/2]
+    [114.5, 960.2, 960.2, 950.5, 714.7, 1565.7,
+     1373.1, 1320.1, 1288.8, 1064.9, 968.6, 408.4],
 
-ideal_pipes = [[],[],[],[],[],[],[]]
+    [114.5, 960.2, 960.2, 950.5, 714.7, 555.7,
+     1373.1, 1320.1, 1288.8, 1064.9, 968.6, 408.4],
 
-for i,j in enumerate(P_stat_max):
-	for k in j:
-		t = (k-14.7) * (30) / (2 * .72 * 52000)
-		if t <= .25:
-			ideal_pipes[i].append(schedules[0]['Name'])
-		elif t <= .624/2:
-			ideal_pipes[i].append(schedules[1]['Name'])
-		elif t <= .75/2:
-			ideal_pipes[i].append(schedules[2]['Name'])
-		elif t <= 1/2:
-			ideal_pipes[i].append(schedules[3]['Name'])
-		elif t <= 1.25/2:
-			ideal_pipes[i].append(schedules[4]['Name'])
-		else:
-			ideal_pipes[i].append('Error: Pressure too great')
-		
+    [114.5, 960.2, 960.2, 950.5, 714.7, 555.7,
+     363.2, 1320.1, 1288.8, 1064.9, 968.6, 408.4],
+
+    [114.5, 960.2, 960.2, 950.5, 714.7, 555.7,
+     363.2, 310.2, 1288.8, 1064.9, 968.6, 408.4],
+
+    [114.5, 960.2, 960.2, 950.5, 714.7, 555.7,
+     363.2, 310.2, 278.8, 1064.9, 968.6, 408.4]
+]
+
+schedules = [{'Name': 'Schedule 5', 'ID': 29.5, 't': .5 / 2,
+              'Area': ((15 ** 2) - ((29.5 / 2) ** 2)) * np.pi},
+             {'Name': 'Schedule 10', 'ID': 29.376, 't': .624 / 2,
+              'Area': ((15 ** 2) - ((29.376 / 2) ** 2)) * np.pi},
+             {'Name': 'STD', 'ID': 29.25, 't': .75 / 2,
+              'Area': ((15 ** 2) - ((29.25 / 2) ** 2)) * np.pi},
+             {'Name': 'Schedule 20', 'ID': 29, 't': 1 / 2,
+              'Area': ((15 ** 2) - ((29 / 2) ** 2)) * np.pi},
+             {'Name': 'Schedule 30', 'ID': 28.75, 't': 1.25 / 2,
+              'Area': ((15 ** 2) - ((28.75 / 2) ** 2)) * np.pi}]
+
+t_vals = [.5 / 2, .624 / 2, .75 / 2, 1 / 2, 1.25 / 2]
+
+ideal_pipes = [[], [], [], [], [], [], []]
+
+for i, j in enumerate(P_stat_max):
+    for k in j:
+        t = (k - 14.7) * (30) / (2 * .72 * 52000)
+        if t <= .25:
+            ideal_pipes[i].append(schedules[0]['Name'])
+        elif t <= .624 / 2:
+            ideal_pipes[i].append(schedules[1]['Name'])
+        elif t <= .75 / 2:
+            ideal_pipes[i].append(schedules[2]['Name'])
+        elif t <= 1 / 2:
+            ideal_pipes[i].append(schedules[3]['Name'])
+        elif t <= 1.25 / 2:
+            ideal_pipes[i].append(schedules[4]['Name'])
+        else:
+            ideal_pipes[i].append('Error: Pressure too great')
+
 # uses itertools combinatorics to create a list of every combination of
 # 3 schedules out of the 5 available (n choose k)
-combined = list(combinations(t_vals,3))
+combined = list(combinations(t_vals, 3))
 
 final_list = []
 
-for h,i in enumerate(P_stat_max):
-	final_list.append([])
-	for j in i:
-		for k in combined:
-			for m in range(3):
-				if k[m] >= (((j-14.7)*30)/(2*.72*52000)):
-					final_list[h].append(k)
-					break
+for h, i in enumerate(P_stat_max):
+    final_list.append([])
+    for j in i:
+        for k in combined:
+            for m in range(3):
+                if k[m] >= (((j - 14.7) * 30) / (2 * .72 * 52000)):
+                    final_list[h].append(k)
+                    break
 
-seen = [{},{},{},{},{},{},{}]
+seen = [{}, {}, {}, {}, {}, {}, {}]
 dupes = []
 
 for i in range(len(final_list)):
-	for x in final_list[i]:
-	    if x not in seen[i]:
-	        seen[i][x] = 1
-	    else:
-	        if seen[i][x] == 1:
-	            dupes.append(x)
-	        seen[i][x] += 1
+    for x in final_list[i]:
+        if x not in seen[i]:
+            seen[i][x] = 1
+        else:
+            if seen[i][x] == 1:
+                dupes.append(x)
+            seen[i][x] += 1
 
 working_list = []
 
-for i,j in enumerate(seen):
-	for x in j:
-		if j[x] == 12:
-			working_list.append((x, i+4))
+for i, j in enumerate(seen):
+    for x in j:
+        if j[x] == 12:
+            working_list.append((x, i + 4))
 
 pipe_vols = []
-				
+
 for group in working_list:
-	for i in range(len(P_stat_max)):
-		vol = 0
-		for section, pressure in zip(sections,P_stat_max[i]):
-			for thickness in group[0]:
-				if (pressure-14.7) < 2496 * thickness:
-					Area_in2 = ((15**2) - ((15-thickness)**2))*np.pi
-					Area_ft2 = Area_in2/144
-					vol_ft3 = Area_ft2 * section
-					break
-				
-			vol += vol_ft3
-		pipe_vols.append((vol, group))
-		
+    for i in range(len(P_stat_max)):
+        vol = 0
+        for section, pressure in zip(sections, P_stat_max[i]):
+            for thickness in group[0]:
+                if (pressure - 14.7) < 2496 * thickness:
+                    Area_in2 = ((15 ** 2) - ((15 - thickness) ** 2)) * np.pi
+                    Area_ft2 = Area_in2 / 144
+                    vol_ft3 = Area_ft2 * section
+                    break
+                # else:
+
+            vol += vol_ft3
+        pipe_vols.append((vol, group))
+
 pipe_vols = sorted(pipe_vols)
 
-pipe_costs   = []
+pipe_costs = []
 pipe_weights = []
 
 for i in pipe_vols:
-	cost = (i[0] * 490 * 3)
-	weight = (i[0] * 490 / 2000) # [tons]
-	pipe_costs.append((cost,i[1]))
-	pipe_weights.append((weight,i[1]))
+    cost = (i[0] * 490 * 3)
+    weight = (i[0] * 490 / 2000)  # [tons]
+    pipe_costs.append((cost, i[1]))
+    pipe_weights.append((weight, i[1]))
