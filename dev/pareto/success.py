5b0ddda5b7351e91bf855cfc5506a7af65d92456
Joseph Amar <joseph.amar1391@gmail.com>
2019-11-04 09:11:06 -0600

    I have no idea what this will do, but I've got a branch further ahead at home.

--- dev/pareto/success.py
+++ dev/pareto/success.py
@@ -1,7 +1,12 @@
-import numpy
+import numpy as np
 
 class People:
-	def __init__(self,talent):
-		self.talent = talent
+	def __init__(self,iq,talent,money,success):
+		self.iq      = iq
+        self.talent  = talent
+        self.money   = money
+        self.success = success
+
+    
 		
 	
